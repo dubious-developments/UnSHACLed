ac2a38ef0ea83e31c1b2c5f8b2e9374817a6c14a
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-24 15:40:57 +0100

    Flesh out model further

--- source/entities/model.ts
+++ source/entities/model.ts
@@ -1,21 +1,38 @@
 import * as Collections from "typescript-collections";
-import { TaskProcessor } from "./taskProcessor";
+import { TaskProcessor, ProcessorTask } from "./taskProcessor";
+
+/**
+ * A model observer: a function that takes a change set as input
+ * and produces a list of tasks to process as output.
+ */
+type ModelObserver = (changeSet: Collections.Set<ModelComponent>)
+    => Array<ProcessorTask<ModelData, ModelTaskMetadata>>;
 
 /**
  * Models the data handled by the UnSHACLed application.
 */
 export class Model {
+    private observers: ModelObserver[];
+
     /**
      * The task processor for the model.
-    */
-    public tasks: TaskProcessor<ModelData, ModelTaskMetadata>;
+     */
+    public readonly tasks: TaskProcessor<ModelData, ModelTaskMetadata>;
 
     /**
      * Creates an empty model.
-    */
-    public constructor() {
+     */
+    public constructor(initialDataGraph: ModelDataGraph) {
         this.tasks = new TaskProcessor<ModelData, ModelTaskMetadata>(
-            new ModelData());
+            new ModelData(initialDataGraph));
+        this.observers = [];
+    }
+
+    /**
+     * Registers an observer with the model.
+     */
+    public registerObserver(observer: ModelObserver) {
+        this.observers.push(observer);
     }
 }
 
@@ -23,7 +40,47 @@ export class Model {
  * Contains a mutable view of a model.
 */
 export class ModelData {
+    /**
+     * The data graph that is at the core of this model.
+     */
+    public readonly dataGraph: ModelDataGraph;
 
+    /**
+     * Creates an empty model.
+    */
+    public constructor(initialDataGraph: ModelDataGraph) {
+        this.dataGraph = new ModelDataGraph(initialDataGraph, () => { });
+    }
+}
+
+/**
+ * Describes the data graph component of the model.
+*/
+export class ModelDataGraph {
+    /**
+     * Creates an empty model data graph that uses a function
+     * to signal changes to the graph.
+     */
+    public constructor(
+        private dataGraph: any,
+        private signalWrite: () => void) {
+    }
+
+    /**
+     * Gets the RDF graph wrapped by this object.
+     */
+    public get graph(): any {
+        return this.dataGraph;
+    }
+
+    /**
+     * Sets the RDF graph wrapped by this object.
+     */
+    public set graph(v: any) {
+        this.dataGraph = v;
+        // The model has changed. Let the rest of the world know.
+        this.signalWrite();
+    }
 }
 
 /**
