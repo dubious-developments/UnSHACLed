39248d1e4fd6173c3c1be1ef0b2603cee551266a
jonathanvdc <jonathan.vdc@outlook.com>
2018-02-28 19:02:02 +0100

    Drop PhantomJS support

--- karma.conf.js
+++ karma.conf.js
@@ -61,9 +61,8 @@ module.exports = function(config) {
                 // https://github.com/litixsoft/karma-detect-browsers/issues/22
                 //
                 // During CI builds, we'll just make sure Chrome's actually installed if
-                // karma-detect-browser advertises it. We'll also test using PhantomJS just
-                // because we can.
-                let undesirables = process.env.CI ? [] : ['PhantomJS', 'Chrome'];
+                // karma-detect-browser advertises it.
+                let undesirables = process.env.CI ? ['PhantomJS'] : ['PhantomJS', 'Chrome'];
 
                 // Remove undesirables if another browser has been detected.
                 for (let i = 0; i < undesirables.length; i++) {
