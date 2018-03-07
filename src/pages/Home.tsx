11e7d7ea2fd6fce0108960b23baef801d040e310
fversche <Freek.Verschelden@UGent.be>
2018-03-07 10:20:48 +0100

    refactor homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { Segment, Container, Image, Header, Grid, Button } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import { Segment} from 'semantic-ui-react';
 import Navbar from '../components/navbarHome';
+import HomeCon from '../components/containerHome';
 
 class Home extends React.Component {
 
     render() {
-        const logo = require('../img/shacl_logo_trans.png');
         return (
             <div className="homePage">
                 <Segment
@@ -22,72 +21,7 @@ class Home extends React.Component {
                     {/*
                      Main Container
                      */}
-                    <Container text={true}>
-
-                        <Image
-                            src={logo}
-                            size="tiny"
-                            verticalAlign="middle"
-                            style={{
-                                marginTop: '2em'
-                            }}
-                        />
-
-                        <Header
-                            as="h1"
-                            inverted={true}
-                            style={{
-                                fontSize: '3em',
-                                fontWeight: 'normal',
-                                marginTop: '2em',
-                            }}
-                        >
-                            {' '} UnSHACLed
-                        </Header>
-
-                        <Header
-                            as="h2"
-                            content="A visual IDE for Semantic Web constraint languages"
-                            inverted={true}
-                            style={{
-                                fontSize: '1.5em',
-                                fontWeight: 'normal',
-                                marginTop: '1.5em',
-                            }}
-                        />
-                        <Grid
-                            verticalAlign="middle"
-                            textAlign="center"
-                            style={{
-                                marginTop: '6.5em',
-                            }}
-                            columns={2}
-                            divided={true}
-
-                        >
-                            <Grid.Column style={{maxWidth: 250}}>
-                                <Button
-                                    inverted={true}
-                                    color="teal"
-                                    size="huge"
-                                >
-                                    <Link
-                                        to="/login"
-                                        style={{color: 'white'}}
-                                    > Login
-                                    </Link>
-                                </Button>
-                            </Grid.Column>
-                            <Grid.Column style={{maxWidth: 250}}>
-                                <Button
-                                    inverted={true}
-                                    size="huge"
-                                > Sign Up
-                                </Button>
-                            </Grid.Column>
-
-                        </Grid>
-                    </Container>
+                    <HomeCon/>
                 </Segment>
             </div>
         );
