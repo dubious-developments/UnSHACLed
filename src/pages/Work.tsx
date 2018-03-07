a3444beb38723cd5eb1e1ad2b9d948113025be41
fversche <Freek.Verschelden@UGent.be>
2018-03-07 10:34:49 +0100

    add non-functional sidebar

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react';
-
 import Navbar from '../components/navbarWork';
+import SideBar from '../components/Sidebar';
 
 class Workspace extends React.Component {
 
     render() {
         return (
-            <div className="workSpace">
-                    <div>
+            <div>
                         <Navbar/>
-                    </div>
+                        <SideBar/>
             </div>
         )
             ;
