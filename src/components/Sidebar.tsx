379970312fabb47b047a57ee0133effdfff2dae8
jjocqu <jens@jocque.be>
2018-03-08 15:34:25 +0100

    create sidebar menus dynamically

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,40 @@ import { Sidebar, Menu, Input, Icon, Image } from 'semantic-ui-react';
 
 class SideBar extends React.Component {
 
+    static SHACLMenuItems = ["Shape", "Node Shape", "Property Shape"];
+    static GeneralMenuItems = ["Arrow", "Rectangle"];
+    static TemplateMenuItems = ["Building", "Person"];
+
+    static menuMapping = new Map([
+        ["SHACL", SideBar.SHACLMenuItems],
+        ["General", SideBar.GeneralMenuItems],
+        ["Template", SideBar.TemplateMenuItems]]
+    );
+
+    constructor(props: string) {
+        super(props);
+    }
+
+    /*
+     * Used for dynamically building the components in the sidebar
+     * In the props must specify the menu type
+     */
+    DynamicMenu(props: any) {
+        var type = props.kind;
+        var items = [];
+        for (var i = 0; i < SideBar.menuMapping.get(type).length; i++) {
+            var key = type + i;
+            console.log(key);
+            items.push(<Menu.Item as="a" content={SideBar.menuMapping.get(type)[i]} key={key}/>);
+        }
+
+        return (
+            <Menu.Menu>
+                {items}
+            </Menu.Menu>
+        );
+    }
+
     render() {
         const logo = require('../img/shacl_logo_trans.png');
         return (
@@ -15,12 +49,11 @@ class SideBar extends React.Component {
                     vertical={true}
                     inverted={true}
                     size="huge"
-                    inline={true}
                 >
-                    <Menu.Item
-                    >
+                    <Menu.Item>
                         <Image src={logo} size="mini" centered={true}/>
                     </Menu.Item>
+                    
                     <Menu.Item>
                         <Input
                             icon={
@@ -35,27 +68,17 @@ class SideBar extends React.Component {
 
                     <Menu.Item >
                         SHACL
-                        <Menu.Menu>
-                            <Menu.Item as="a" content="Shape"/>
-                            <Menu.Item as="a" content="Node Shape"/>
-                            <Menu.Item as="a" content="Property Shape"/>
-                        </Menu.Menu>
+                        <this.DynamicMenu kind="SHACL" />
                     </Menu.Item>
 
                     <Menu.Item>
                         General
-                        <Menu.Menu>
-                            <Menu.Item as="a" content="Arrow"/>
-                            <Menu.Item as="a" content="Rectangle"/>
-                        </Menu.Menu>
+                        <this.DynamicMenu kind="General" />
                     </Menu.Item>
 
                     <Menu.Item>
                         Template
-                        <Menu.Menu>
-                            <Menu.Item as="a" content="Building"/>
-                            <Menu.Item as="a" content="Person"/>
-                        </Menu.Menu>
+                        <this.DynamicMenu kind="Template" />
                     </Menu.Item>
                 </Sidebar>
 
