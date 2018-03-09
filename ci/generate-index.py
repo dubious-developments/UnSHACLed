6b65c5a635589ddf9c7f57901d394189091ac840
jonathanvdc <jonathan.vdc@outlook.com>
2018-03-09 18:07:16 +0100

    Support deploying pull request builds

--- ci/generate-index.py
+++ ci/generate-index.py
@@ -36,7 +36,7 @@ def generate_index(name_directory_tuples):
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>UnSHACLed</title>
+    <title>UnSHACLed builds</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
