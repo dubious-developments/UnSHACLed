a5ab5e5fca9452e950f5a1c283c4cfe2ad321d21
jonathanvdc <jonathan.vdc@outlook.com>
2018-03-09 17:13:53 +0100

    Move deployment to after_script section

--- ci/deploy-to-gh-pages.sh
+++ ci/deploy-to-gh-pages.sh
@@ -10,17 +10,27 @@ ACCESS_TOKEN=$1
 # The second parameter is the directory to store the build in.
 TARGET_DIRECTORY=$2
 
-pushd $CURRENT_DIR
+# The third parameter is the (human-readable) name of the build.
+NAME=$3
+
+# The fourth parameter is the build's version.
+VERSION=$4
+
+pushd "$CURRENT_DIR"
 
 # Clone the GitHub pages repository.
 git clone https://github.com/dubious-developments/dubious-developments.github.io
 
 # Delete the old build, if any.
-rm -rf dubious-developments.github.io/$TARGET_DIRECTORY
+rm -rf "dubious-developments.github.io/$TARGET_DIRECTORY"
 
 # Copy the build to the GitHub pages directory.
-mkdir dubious-developments.github.io/$TARGET_DIRECTORY
-cp -r build/* dubious-developments.github.io/$TARGET_DIRECTORY
+mkdir "dubious-developments.github.io/$TARGET_DIRECTORY"
+cp -r build/* "dubious-developments.github.io/$TARGET_DIRECTORY"
+
+# Create build name and version files.
+echo "$NAME" > "dubious-developments.github.io/$TARGET_DIRECTORY/build-name"
+echo "$VERSION" > "dubious-developments.github.io/$TARGET_DIRECTORY/version"
 
 pushd dubious-developments.github.io
 
