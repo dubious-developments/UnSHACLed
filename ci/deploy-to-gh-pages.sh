eec2af9e065a2d3ab6f1be48ef00b295395b9492
jonathanvdc <jonathan.vdc@outlook.com>
2018-03-09 17:00:44 +0100

    Be more secretive about access token

--- ci/deploy-to-gh-pages.sh
+++ ci/deploy-to-gh-pages.sh
@@ -5,8 +5,7 @@
 set -e
 
 CURRENT_DIR=$(dirname $0)
-
-ACCESS_TOKEN=$(echo $1 | tr '[N-ZA-Mn-za-m]' '[A-Za-z]')
+ACCESS_TOKEN=$1
 
 # The second parameter is the directory to store the build in.
 TARGET_DIRECTORY=$2
@@ -34,7 +33,7 @@ git -c user.name='Dubious Spongebot' \
     commit -m "Deploy $TARGET_DIRECTORY"
 
 # Push the changes.
-git push https://dubious-spongebot:$ACCESS_TOKEN@github.com/dubious-developments/dubious-developments.github.io master
+git push https://dubious-spongebot:$ACCESS_TOKEN@github.com/dubious-developments/dubious-developments.github.io master &2> /dev/null
 
 popd
 
