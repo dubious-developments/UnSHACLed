ccd18d11a4f6bf9c65a6e575f51eae7613c4d08c
fversche <Freek.Verschelden@UGent.be>
2018-03-12 11:50:26 +0100

    functional treeview component

--- src/components/treeView.tsx
+++ src/components/treeView.tsx
@@ -1,10 +1,47 @@
 import * as React from 'react';
+import {Treebeard} from 'react-treebeard';
+
+const data = {
+    name: 'root',
+    children: [
+        {
+            name: 'parent',
+            children: [
+                { name: 'child1' },
+                { name: 'child2' }
+            ]
+        }
+    ]
+};
 
 class Tree extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {};
+        this.onToggle = this.onToggle.bind(this);
+    }
+
+    onToggle(node: any, toggled: any) {
+        if (this.state.cursor) {
+            this.state.cursor.active = false;
+        }
+        node.active = true;
+        if (node.children) {
+            node.toggled = toggled;
+        }
+        this.setState({
+            cursor: node
+        });
+    }
+
     render() {
         return (
             <div>
-                <h1> Tree </h1>
+                <Treebeard
+                    data={data}
+                    onToggle={this.onToggle}
+                />
             </div>
         );
     }
