cfd80ddccb542a4b1ea77c8e1678883a659011eb
jonathanvdc <jonathan.vdc@outlook.com>
2018-04-01 12:37:45 +0200

    Fix babel-config.js style

--- babel-config.js
+++ babel-config.js
@@ -1,15 +1,15 @@
 module.exports = {
     presets: [
-        ['env', {
-            'targets': {
-                'browsers': [
-                    '>1%',
-                    'last 4 versions',
-                    'Firefox ESR',
-                    'Explorer 11',
-                    'not ie < 9']
+        ["env", {
+            "targets": {
+                "browsers": [
+                    ">1%",
+                    "last 4 versions",
+                    "Firefox ESR",
+                    "Explorer 11",
+                    "not ie < 9"]
             }
         }]
     ],
     babelrc: false
-}
+};
