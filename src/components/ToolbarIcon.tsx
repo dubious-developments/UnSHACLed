c6e7f25d0d00eac359269ed3127174377a07a1d0
fversche <Freek.Verschelden@UGent.be>
2018-04-29 16:06:52 +0200

    add react component for toolbar icons

---
+++ src/components/ToolbarIcon.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import {Menu, Input, Popup} from 'semantic-ui-react';
+import {SidebarProps} from './interfaces/interfaces';
+
+class ToolbarIcon extends React.Component<SidebarProps, any> {
+
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+        };
+
+    }
+
+    render() {
+        return (
+            <div>
+                Test
+            </div>
+        );
+    }
+}
+
+export default ToolbarIcon;
