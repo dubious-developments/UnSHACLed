6d2fb130ba6cbb9455bd3e6a71992c260f81813f
fversche <Freek.Verschelden@UGent.be>
2018-04-29 21:16:46 +0200

    add component for sidebar popup

---
+++ src/components/sidebarPopup.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import {Menu, Popup, Grid, Header, Divider, Image} from 'semantic-ui-react';
+import {ToolbarIconProps} from './interfaces/interfaces';
+
+/*
+    Component used to create pop-up for draggable elements within the sidebar
+    Requires several props from the parent, which can be found in interfaces.d.ts
+
+ */
+class SidebarPopup extends React.Component<ToolbarIconProps, any> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Popup
+                trigger={this.props.trigger}
+                flowing={true}
+                hoverable={true}
+                size={this.props.p_size}
+                position={this.props.p_position}
+                inverted={true}
+            >
+                <Grid centered={true} divided={true}>
+                    <Grid.Column textAlign='center'>
+                        <Header as='h4'> {this.props.headertitle} </Header>
+                        <Divider/>
+                        <Image
+                            src={logo}
+                            size="mini"
+                            centered={true}
+                        />
+                    </Grid.Column>
+                </Grid>
+            </Popup>
+        );
+    }
+}
+
+export default SidebarPopup;
