e84ea41526e8566380f78f260e267d021c8a869b
jonathanvdc <jonathan.vdc@outlook.com>
2018-05-02 16:18:44 +0200

    Use Pixie to parse command-line parameters

---
+++ selenium/Options.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Pixie.Options;
+
+namespace SeleniumTests
+{
+    /// <summary>
+    /// A collection of options for the test runner.
+    /// </summary>
+    public static class Options
+    {
+        /// <summary>
+        /// A pseudo-option for the URI to query. 
+        /// </summary>
+        /// <value>A pseudo-option.</value>
+        public static readonly Option Uri =
+            ValueOption.CreateStringOption(
+                OptionForm.Long("uri"),
+                "")
+            .WithDescription("Specifies the URI at which UnSHACLed is hosted.")
+            .WithParameter(new SymbolicOptionParameter("uri"));
+
+        /// <summary>
+        /// The 'help' option.
+        /// </summary>
+        /// <returns>An option.</returns>
+        public static readonly Option Help =
+            FlagOption.CreateFlagOption(
+                OptionForm.Short("h"),
+                OptionForm.Long("help"))
+            .WithDescription("Prints a help message.");
+
+        /// <summary>
+        /// A read-only list of all options accepted by the function testing
+        /// program.
+        /// </summary>
+        public static readonly IReadOnlyList<Option> All = new Option[]
+        {
+            Help
+        };
+    }
+}
+
--- selenium/Program.cs
+++ selenium/Program.cs
@@ -2,12 +2,66 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using Pixie;
+using Pixie.Markup;
+using Pixie.Options;
+using Pixie.Terminal;
+using Pixie.Transforms;
 
 namespace SeleniumTests
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
+        {
+            bool errorEncountered = false;
+
+            // Acquire a log for when things go sideways.
+            var log = new TransformLog(
+                TerminalLog.Acquire(),
+                entry => DiagnosticExtractor.Transform(entry, "selenium-tests"),
+                entry =>
+                {
+                    if (entry.Severity == Severity.Error)
+                    {
+                        errorEncountered = true;
+                    }
+                    return entry;
+                });
+
+            // Parse command-line arguments.
+            var optParser = new GnuOptionSetParser(Options.All, Options.Uri);
+
+            // Actually parse the options.
+            var parsedOptions = optParser.Parse(args, log);
+
+            if (errorEncountered)
+            {
+                // Ouch. Command-line arguments were bad. Stop testing now.
+                return 1;
+            }
+            else if (parsedOptions.GetValue<bool>(Options.Help))
+            {
+                // Print a cute little help message (to stdout instead of stderr).
+                var helpLog = TerminalLog.AcquireStandardOutput();
+                helpLog.Log(
+                    new Pixie.LogEntry(
+                        Severity.Message,
+                        new HelpMessage(
+                            "Runs UnSHACLed's functional tests.",
+                            "selenium-tests uri [options...]",
+                            Options.All)));
+                return 0;
+            }
+
+            Run(log);
+
+            // If things went swimmingly, then return a zero exit code.
+            // Otherwise, let the world know that something is wrong.
+            return errorEncountered ? 1 : 0;
+        }
+
+        private static void Run(ILog log)
         {
             // Create a new instance of the Firefox driver.
             // Note that it is wrapped in a using clause so that the browser is closed 
@@ -42,7 +96,11 @@ namespace SeleniumTests
                 wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
 
                 // Should see: "Cheese - Google Search" (for an English locale)
-                Console.WriteLine("Page title is: " + driver.Title);
+                log.Log(
+                    new Pixie.LogEntry(
+                        Severity.Message,
+                        "page title",
+                        driver.Title));
             }
         }
     }
