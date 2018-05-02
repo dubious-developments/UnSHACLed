177c5bfd45b327db533cd0c73204e71ea2066d40
jonathanvdc <jonathan.vdc@outlook.com>
2018-05-02 20:58:17 +0200

    Create a framework for defining Selenium test cases

--- selenium/Program.cs
+++ selenium/Program.cs
@@ -12,11 +12,17 @@ using Pixie.Markup;
 using Pixie.Options;
 using Pixie.Terminal;
 using Pixie.Transforms;
+using SeleniumTests.Tests;
 
 namespace SeleniumTests
 {
     public static class Program
     {
+        /// <summary>
+        /// A sequence of all test cases to run.
+        /// </summary>
+        private static readonly IEnumerable<TestCase> TestCases = SanityChecks.All;
+
         public static int Main(string[] args)
         {
             bool errorEncountered = false;
@@ -199,29 +205,14 @@ namespace SeleniumTests
             {
                 using (IWebDriver driver = builder())
                 {
-                    //Notice navigation is slightly different than the Java version
-                    //This is because 'get' is a keyword in C#
-                    driver.Navigate().GoToUrl(uri);
+                    foreach (var testCase in TestCases)
+                    {
+                        // Always navigate to the front page first.
+                        driver.Navigate().GoToUrl(uri);
 
-                    // // Find the text input element by its name
-                    // IWebElement query = driver.FindElement(By.Name("q"));
-
-                    // // Enter something to search for
-                    // query.SendKeys("Cheese");
-
-                    // // Now submit the form. WebDriver will find the form for us from the element
-                    // query.Submit();
-
-                    // // Google's search is rendered dynamically with JavaScript.
-                    // // Wait for the page to load, timeout after 10 seconds
-                    // var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                    // wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
-
-                    log.Log(
-                        new Pixie.LogEntry(
-                            Severity.Message,
-                            "page title",
-                            driver.Title));
+                        // Then run the actual test case.
+                        testCase.Run(driver, log);
+                    }
                 }
             }
         }
