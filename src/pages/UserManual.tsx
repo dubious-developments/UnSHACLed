efc947570cb29ed0e3427ed0e875a66eb73479b1
fversche <Freek.Verschelden@UGent.be>
2018-05-09 17:38:31 +0200

    update user manual

--- src/pages/UserManual.tsx
+++ src/pages/UserManual.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Header, Menu, Container, Image} from 'semantic-ui-react';
+import {Header, Menu, Container, Image, List} from 'semantic-ui-react';
 
 class Home extends React.Component {
 
@@ -22,6 +22,44 @@ class Home extends React.Component {
                 <Container text={true} style={{marginTop: '7em'}}>
                     <Header as='h1'> UnSHACLed Online User Manual</Header>
                     <p>A visual IDE for Semantic Web constraint languages </p>
+                    <p><b> Table of Contents </b></p>
+                    {/* Table of contents */}
+                    <List ordered={true}>
+                        <List.Item>
+                            <a>Introduction</a>
+                            <List.List>
+                                <List.Item as='a'>Semantic Web</List.Item>
+                                <List.Item as='a'>SHACL</List.Item>
+                                <List.Item as='a'>UnSHACLed</List.Item>
+                            </List.List>
+                        </List.Item>
+                        <List.Item as='a'>
+                            <a> Getting Started</a>
+                            <List.List>
+                                <List.Item as='a'>Create an account</List.Item>
+                                <List.Item as='a'>Login to UnSHACLed</List.Item>
+                                <List.Item as='a'>Logout</List.Item>
+                            </List.List>
+                        </List.Item>
+                        <List.Item>
+                            <a>File Handling</a>
+                            <List.List>
+                                <List.Item as='a'> Open local files</List.Item>
+                                <List.Item as='a'>Save local files</List.Item>
+                                <List.Item as='a'>Open files from your account </List.Item>
+                                <List.Item as='a'>Save files to your account </List.Item>
+                            </List.List>
+                        </List.Item>
+                        <List.Item>
+                            <a>Editor functionality</a>
+                            <List.List>
+                                <List.Item as='a'>Semantic Web</List.Item>
+                                <List.Item as='a'>SHACL</List.Item>
+                                <List.Item as='a'>UnSHACLed</List.Item>
+                            </List.List>
+                        </List.Item>
+                    </List>
+
                 </Container>
 
             </div>
