41c9e2256893b068d8b2b9ade873ac1577c33e37
fversche <Freek.Verschelden@UGent.be>
2018-05-10 23:23:07 +0200

    add user info request method + documentation

--- src/requests/RequestModule.tsx
+++ src/requests/RequestModule.tsx
@@ -1,48 +1,54 @@
 import axios from 'axios';
 
-/*
- RequestModule class which serves several http requests functions for interaction
- with bot the collaboration server as well as the GitHub API to other components
- of the application.
+/**
+ * RequestModule class which serves several http requests functions for interaction
+ * with bot the collaboration server as well as the GitHub API to other components
+ * of the application.
  */
-class RequestModule {
-    /* Collaboration Server interaction */
 
-    /*
-        Method to receive a token from the collaboration server.
-        A POST request is used to obtain the token in a plain text from the collaboration server.
-        The obtained token is used for further authentication.
+class RequestModule {
+    
+    /**
+     * Method to receive a token from the collaboration server.
+     * A POST request is used to obtain the token in a plain text from the collaboration server.
+     * The obtained token is used for further authenticatio
+     * @returns {Promise<AxiosResponse<any>>}, Return the obtained token through a promise.
      */
     static getToken() {
         return axios.post('http://193.190.127.184:8042/auth/request-token')
             .then(res => res.data);
     }
-
-    /* Method to start authentication process through the collaboration server using
-    a GET request with the earlier obtained token. The collaboration server will redirect
-    to the GitHub login page and redirect if logged in succesfully.
+    /**
+     * Method to start authentication process through the collaboration server using
+     * a GET request with the earlier obtained token. The collaboration server will redirect
+     * to the GitHub login page and redirect if logged in succesfully
+     * @param token: token associated with authenticated user and obtained using getToken().
      */
     static AuthWithToken(token: any) {
         window.open('http://193.190.127.184:8042/auth/auth/' + token, "_blank",
             "top=100,left=500,width=500,height=1000");
     }
 
-    /* Method to check wether or not a user with an obtained token is authenticated using a
-       GET request to the collaboration server including the previously obtained token
-        Returns either true or false
+    /**
+     * Method to check wether or not a user with an obtained token is authenticated using a
+     * GET request to the collaboration server including the previously obtained token
+     * @param token: token associated with authenticated user and obtained using getToken().
+     * @returns {Promise<AxiosResponse<any>>}, Returns either true or false through a promise.
      */
-    static isAuthenticated(this: any, token: any) {
+    static isAuthenticated(token: any) {
         return axios.get('http://193.190.127.184:8042/auth/is-authenticated/' + token)
             .then(res => res.data);
     }
 
-    /* GitHub API*/
-    static getUserInfo(user: any) {
-
-    }
-
-    static getUserRepos(user: any) {
-
+    /**
+     * Method to obtain user info for an authenticated user
+     * @param type: specifies type of user info, can be 'login', 'name' or 'email'.
+     * @param token: token associated with authenticated user and obtained using getToken().
+     * @returns {Promise<AxiosResponse<any>>}, Returns the requested user info through a promise.
+     */
+    static getUserInfo(type: any, token: any) {
+        return axios.get('http://193.190.127.184:8042/user/' + type + '/' + token)
+            .then(res => res.data);
     }
 }
 
