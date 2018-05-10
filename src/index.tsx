60845ccf84a7a484a274949ec450380d4502392f
fversche <Freek.Verschelden@UGent.be>
2018-05-10 23:59:22 +0200

    add basic global store

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,22 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import Routes from "./components/Routes";
 import 'semantic-ui-css/semantic.min.css';
+import {createStore} from 'redux';
+
+function reducer() {
+    return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.render(
-    <HashRouter >
-        <Routes />
+    <HashRouter>
+        <Routes/>
     </HashRouter>,
     document.getElementById('root'));
 registerServiceWorker();
