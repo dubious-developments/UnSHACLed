d6ae491fd8ba054aa2cedd6a9a44abcc538402e6
fversche <Freek.Verschelden@UGent.be>
2018-05-11 01:01:10 +0200

    add all needed reducers

--- src/index.tsx
+++ src/index.tsx
@@ -6,25 +6,36 @@ import {HashRouter} from 'react-router-dom';
 import Routes from "./components/Routes";
 import 'semantic-ui-css/semantic.min.css';
 import {createStore, combineReducers} from 'redux';
-import userReducer from './redux/reducers/userReducer';
+import {userNameReducer, userLoginReducer, userEmailReducer, userTokenReducer} from './redux/reducers/userReducer';
 
 const allReducers = combineReducers(
     {
-        user: userReducer
+        name: userNameReducer,
+        login: userLoginReducer,
+        email: userEmailReducer,
+        token: userTokenReducer
     }
 );
 const store = createStore(allReducers);
 
 const updateUserAction = {
-    type: 'updateUserAction',
+    type: 'updateUsername',
     payload: {
-        user: 'name of user'
+        name: 'name of user'
+    }
+};
+
+const updateUserlogin = {
+    type: 'updateUserLogin',
+    payload: {
+        login: 'here comes login'
     }
 };
 
 console.log(store.getState());
 
 store.dispatch(updateUserAction);
+store.dispatch(updateUserlogin);
 
 console.log(store.getState());
 
--- src/redux/reducers/userReducer.tsx
+++ src/redux/reducers/userReducer.tsx
@@ -1,7 +1,34 @@
-export default function userReducer(state: any = '', action: any) {
+export function userNameReducer(state: any = '', action: any) {
     switch (action.type) {
-        case 'updateUserAction':
-            return action.payload.user;
+        case 'updateUsername':
+            return action.payload.name;
+        default :
+            return state;
+    }
+}
+
+export function userLoginReducer(state: any = '', action: any) {
+    switch (action.type) {
+        case 'updateUserLogin':
+            return action.payload.login;
+        default :
+            return state;
+    }
+}
+
+export function userEmailReducer(state: any = '', action: any) {
+    switch (action.type) {
+        case 'updateUserEmail':
+            return action.payload.email;
+        default :
+            return state;
+    }
+}
+
+export function userTokenReducer(state: any = '', action: any) {
+    switch (action.type) {
+        case 'updateUserToken':
+            return action.payload.token;
         default :
             return state;
     }
