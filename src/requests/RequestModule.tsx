f50c51c79158f62dde38f13b4944bad531a61915
fversche <Freek.Verschelden@UGent.be>
2018-05-16 23:08:34 +0200

    implement api method to check for lock

--- src/requests/RequestModule.tsx
+++ src/requests/RequestModule.tsx
@@ -109,21 +109,37 @@ class RequestModule {
             .then(res => console.log(res));
     }
 
-    // {owner}/{repoName}/{token}/{filePath}
+    // TODO replace lock method with one generic method
     /**
-     *
+     * Method to check wether user has a lock on a file.
      * @param repoOwner: owner of repository where file is located
      * @param repoName: name of repository
      * @param token: token associated with authenticated user and obtained using getToken().
      * @param filePath: path of file to be created updated
      * @returns {Promise<AxiosResponse<any>>}, return true if lock has been granted, false otherwise through a Promise.
      */
-    static getLock(repoOwner: any, repoName: any, token: any, filePath: any) {
+    static hasLock(repoOwner: any, repoName: any, token: any, filePath: any) {
         const target =
             'http://193.190.127.184:8042/repo/has-lock/' + repoOwner + '/' + repoName + '/' + token + '/' + filePath;
         return axios.get(target)
             .then(res => res.data);
     }
+
+    /**
+     * Method to request a lock on a file.
+     * @param repoOwner: owner of repository where file is located
+     * @param repoName: name of repository
+     * @param token: token associated with authenticated user and obtained using getToken().
+     * @param filePath: path of file to be created updated
+     * @returns {Promise<AxiosResponse<any>>}, return true if lock has been granted, false otherwise through a Promise.
+     */
+    static requestLock(repoOwner: any, repoName: any, token: any, filePath: any) {
+        const target = 'http://193.190.127.184:8042/repo/request-lock/'
+            + repoOwner + '/' + repoName + '/' + token + '/' + filePath;
+        return axios.post(target)
+            .then(res => res.data);
+    }
+
 }
 
 export default RequestModule;
