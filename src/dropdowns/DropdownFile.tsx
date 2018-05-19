63d951bd0b7f29e96aa685e6397ba1fdf8c866b2
fversche <Freek.Verschelden@UGent.be>
2018-05-19 21:49:07 +0200

    add input file pop-up for workspace

--- src/dropdowns/DropdownFile.tsx
+++ src/dropdowns/DropdownFile.tsx
@@ -4,6 +4,8 @@ import {DropdownFileProps} from '../components/interfaces/interfaces';
 import RepoModal from '../modals/RepoModal';
 import NewModal from '../modals/NewModal';
 import {connect} from 'react-redux';
+import {ModelComponent} from "../entities/modelTaskMetadata";
+import {FileModule} from "../persistence/fileDAO";
 
 /**
  Component used to create a dropdown component for the file toolbar option
@@ -11,6 +13,8 @@ import {connect} from 'react-redux';
 
  */
 class DropdownFile extends React.Component<DropdownFileProps & any, any> {
+
+    allowedExtensions = ".n3,.ttl,.rdf";
 
     constructor(props: any) {
         super(props);
@@ -30,6 +34,7 @@ class DropdownFile extends React.Component<DropdownFileProps & any, any> {
         this.saveWorkspace = this.saveWorkspace.bind(this);
         this.saveFileToAccount = this.saveFileToAccount.bind(this);
         this.getRepoFromFileName = this.getRepoFromFileName.bind(this);
+        this.clickWorkspace = this.clickWorkspace.bind(this);
     }
 
     /**
@@ -168,11 +173,36 @@ class DropdownFile extends React.Component<DropdownFileProps & any, any> {
     }
 
     /**
+     * Method that will invoke the html input to load a workspace.
+     */
+    clickWorkspace() {
+        let input = document.getElementById("loadWorkspace");
+        if (input) {
+            input.click();
+        } else {
+            console.log("Could not find element 'loadWorkspace'");
+        }
+    }
+
+    /**
      * Method to load a workspace. The user need to provide a valid
      * workspace formatted file obtained by saving the workspace before hand.
      */
     loadWorkspace() {
+        let input = (document.getElementById("loadWorkspace") as HTMLInputElement);
 
+        if (input) {
+            let files = input.files;
+            if (files) {
+                if (files[0]) {
+                    console.log(files[0]);
+                }
+            } else {
+                console.log("error: no files found");
+            }
+        } else {
+            console.log("error: could not find loadWorkspace button");
+        }
     }
 
     /**
@@ -254,10 +284,19 @@ class DropdownFile extends React.Component<DropdownFileProps & any, any> {
                             <Dropdown.Menu content={<this.getGitHubFiles/>}/>
                         </Dropdown>
                         <Dropdown.Divider/>
-                        <Dropdown.Item text='Load workspace' onClick={this.loadWorkspace}/>
+                        <Dropdown.Item text='Load workspace' onClick={this.clickWorkspace}/>
                         <Dropdown.Item text='Save workspace' onClick={this.saveWorkspace}/>
+
                     </Dropdown.Menu>
                 </Dropdown>
+
+                <input
+                    onChange={this.loadWorkspace}
+                    type="file"
+                    id="loadWorkspace"
+                    style={{"display": "none"}}
+                />
+
                 {repoVisible ?
                     <RepoModal
                         visible={repoVisible}
